9c2c9bbdc4f4dfb28418f626e073a15197d25ea2
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-01 23:58:32 +0530

    feat(env):implemented the checks for env (db_url),and for email verification (otp)

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,6 +3,34 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import dotenv from "dotenv";
 dotenv.config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
+}
+
+if (!process.env.GOOGLE_CLIENT_ID) {
+  throw new Error(
+    "GOOGLE_CLIENT_ID is defined in environment variables, but it is not used in this file",
+  );
+}
+
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error(
+    "GOOGLE_CLIENT_SECRET is defined in environment variables, but it is not used in this file",
+  );
+}
+
+if (!process.env.REFRESH_TOKEN_G) {
+  throw new Error(
+    "REFRESH_TOKEN_SECRET is defined in environment variables, but it is not used in this file",
+  );
+}
+
+if (!process.env.GOGGLE_USER) {
+  throw new Error(
+    "GOOGLE_USER is defined in environment variables, but it is not used in this file",
+  );
+}
+
 const client = new PrismaClient({
   adapter: new PrismaPg({
     connectionString: process.env.DATABASE_URL,
