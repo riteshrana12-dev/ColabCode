a360d2c4907e2bb04e712bbdbead3182f663d86f
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-02 02:25:04 +0530

    feat(auth): add JWT middleware with user/session extraction
    - Implement middleware to validate Authorization header token
    - Decode JWT payload and attach userId and sessionId to request
    - Return 401 for missing/invalid token and 500 for server errors
    - Ensure next() is called for authorized requests

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { decodeidI } from "../types";
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.header("Authorization")?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET as string);
+
+    req.userId = (decode as decodeidI).id;
+    req.sessionId = (decode as decodeidI).sessionId;
+
+    next();
+
+    return res.status(200).json({
+      message: "Authorized",
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export default authMiddleware;
