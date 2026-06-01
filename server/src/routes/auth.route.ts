0156bf5c26828044c3d2cb8aae1d27c6c1edf4e2
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-02 01:01:51 +0530

    feat(auth): configure authentication routes

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -5,5 +5,8 @@ const authRouter = Router();
 authRouter.post("/signup", authController.signup);
 authRouter.get("/refreshtoken", authController.refreshToken);
 authRouter.get("/logout", authController.logOut);
+authRouter.get("/logoutall", authController.logOutAll);
+authRouter.post("/signin", authController.signIn);
+authRouter.post("/verifyEmail", authController.verifyEmail);
 
 export default authRouter;
