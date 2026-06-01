663eef798559d2a4789d7ac1898fed44804bd34f
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-01 23:59:56 +0530

    feat(route):Initialize the auth routes for signup , refresh token , logout

---
+++ server/src/routes/auth.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import authController from "../controllers/auth.controller";
+const authRouter = Router();
+
+authRouter.post("/signup", authController.signup);
+authRouter.get("/refreshtoken", authController.refreshToken);
+authRouter.get("/logout", authController.logOut);
+
+export default authRouter;
