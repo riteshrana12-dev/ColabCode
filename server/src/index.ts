83012ef95a7c3537b39831e2d942f8bbfb324da6
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-05 20:46:10 +0530

    feat(server):running websocket server and express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,23 @@
-import app from "./app";
+import { httpServer } from "./http";
+import { initWebSocket } from "./websocket/server";
+import dotenv from "dotenv";
 
-(() => {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running on port 3000");
+dotenv.config();
+
+// create one httpServer from express app
+
+const start = async () => {
+  // init Socket.io (attaches to httpServer internally)
+  await initWebSocket();
+
+  const PORT = process.env.PORT || 3000;
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
-})();
+};
+
+start().catch((err) => {
+  console.error("Failed to start server:", err);
+  process.exit(1);
+});
