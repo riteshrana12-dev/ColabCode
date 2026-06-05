c1def4de0dcff7ea97e083c854782731846678e0
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-05 20:42:15 +0530

    refractor(route):done route arrangement

--- server/src/routes/room.route.ts
+++ server/src/routes/room.route.ts
@@ -5,9 +5,10 @@ import roomController from "../controllers/room.controller";
 const roomRouter = Router();
 
 roomRouter.post("/create", authMiddleware, roomController.createRoom);
-roomRouter.post("/delete", authMiddleware, roomController.deleteRoom);
-roomRouter.post("/rename", authMiddleware, roomController.renameRoom);
 roomRouter.post("/join", authMiddleware, roomController.joinRoom);
 roomRouter.post("/leave", authMiddleware, roomController.leaveRoom);
+roomRouter.post("/delete", authMiddleware, roomController.deleteRoom);
+roomRouter.post("/rename", authMiddleware, roomController.renameRoom);
+roomRouter.get("/my-rooms", authMiddleware, roomController.getMyRooms);
 
 export default roomRouter;
