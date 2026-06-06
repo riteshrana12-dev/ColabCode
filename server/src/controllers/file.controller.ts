6eade5d2d892a636921ac4d89d2abe20c7a9e823
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-07 01:21:13 +0530

    feat(file): implement createFile controller with parent validation and duplicate name checks

--- server/src/controllers/file.controller.ts
+++ server/src/controllers/file.controller.ts
@@ -26,4 +26,56 @@ const getFileTree = async (req: Request, res: Response) => {
   }
 };
 
-export default { getFileTree };
+const createFile = async (req: Request, res: Response) => {
+  const { roomId, name, type = "file", parentId = null } = req.body;
+
+  try {
+    if (!name || name.trim() === "") {
+      return res.status(400).json({
+        message: "Name is required",
+      });
+    }
+
+    if (parentId) {
+      const parent = await client.file.findUnique({ where: { id: parentId } });
+
+      if (!parent || parent.type !== "folder") {
+        return res.status(400).json({
+          mesasge: "Parent folder not found",
+        });
+      }
+    }
+
+    const existing = await client.file.findFirst({
+      where: { roomId, parentId, name },
+    });
+
+    if (!existing) {
+      return res.status(400).json({
+        message: `A ${type} with that name already exist`,
+      });
+    }
+
+    const file = await client.file.create({
+      data: {
+        name,
+        type,
+        parentId,
+        roomId,
+        language: type === "file" ? getLanguage(name) : "plaintext",
+      },
+    });
+
+    return res.status(200).json({
+      message: `${type} created`,
+      file,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export default { getFileTree, createFile };
