9efe9f4b2621522f4d24482fe4ab97986958eb5d
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-16 21:51:05 +0530

    refractor(route): refresh token route for get and post

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -4,6 +4,7 @@ const authRouter = Router();
 
 authRouter.post("/signup", authController.signup);
 authRouter.get("/refreshtoken", authController.refreshToken);
+authRouter.post("/refreshtoken", authController.refreshToken);
 authRouter.post("/logout", authController.logOut);
 authRouter.post("/logoutall", authController.logOutAll);
 authRouter.post("/signin", authController.signIn);
