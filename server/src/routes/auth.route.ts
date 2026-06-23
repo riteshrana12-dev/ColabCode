37709b44ed4811b1a92f0645f4b88b5ea37fd672
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-24 01:56:10 +0530

    feat(route):added the delete route

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller";
+import authMiddleware from "../middleware/auth.middleware";
 const authRouter = Router();
 
 authRouter.post("/signup", authController.signup);
@@ -7,6 +8,11 @@ authRouter.get("/refreshtoken", authController.refreshToken);
 authRouter.post("/refreshtoken", authController.refreshToken);
 authRouter.post("/logout", authController.logOut);
 authRouter.post("/logoutall", authController.logOutAll);
+authRouter.post(
+  "/delete-account",
+  authMiddleware,
+  authController.deleteAccount,
+);
 authRouter.post("/signin", authController.signIn);
 authRouter.post("/verifyEmail", authController.verifyEmail);
 
