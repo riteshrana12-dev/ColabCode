d7f0fe2e8ddd593673abeea3f8967046a75046fd
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-24 17:11:01 +0530

    ui(icon):Mic icon

---
+++ client/src/components/video/icon/MicIcon.tsx
@@ -0,0 +1,15 @@
+export default function MicIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />
+      <path d="M19 10v2a7 7 0 0 1-14 0v-2M12 19v4M8 23h8" />
+    </svg>
+  );
+}
