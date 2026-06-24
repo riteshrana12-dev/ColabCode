f35ad5e7c25361431a56547b8258cc355810e3c5
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-24 17:09:21 +0530

    ui(icon):camera icon for call

---
+++ client/src/components/video/icon/CamIcon.tsx
@@ -0,0 +1,15 @@
+export default function CamIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path d="M23 7l-7 5 7 5V7z" />
+      <rect x="1" y="5" width="15" height="14" rx="2" ry="2" />
+    </svg>
+  );
+}
