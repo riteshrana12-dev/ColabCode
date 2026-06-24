27d8dda38656f3fd39e52304521e578c5bde5c4b
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-25 01:46:03 +0530

    feat(auth): add verifyEmail function with OTP verification and navigation

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import api from "../services/api";
 import { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
 
 export default function useAuth() {
+  const navigate = useNavigate();
+  const setAuth = useAuthStore((s) => s.setAuth);
   const [loading, setLoading] = useState(false);
   const [step, setStep] = useState<"register" | "verify">("register");
   const [error, setError] = useState("");
@@ -25,8 +29,28 @@ export default function useAuth() {
     }
   };
 
+  const verifyEmail = async (email: string, otp: string) => {
+    try {
+      setLoading(true);
+      const res = await api.post("/auth/verifyEmail", { email, otp });
+      setAuth(res.data.user, res.data.accessToken);
+      navigate("/dashboard");
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        setError(error.response?.data?.message || "Something went wrong");
+      } else if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("Unexpected error");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
     signUp,
+    verifyEmail,
     step,
     loading,
     error,
