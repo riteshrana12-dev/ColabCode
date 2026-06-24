6a7a37735803b9a289c2159834003035a2602072
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-24 17:11:42 +0530

    ui(icon):mic off icon

---
+++ client/src/components/video/icon/MicOffIcon.tsx
@@ -0,0 +1,16 @@
+export default function MicOffIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <line x1="1" y1="1" x2="23" y2="23" />
+      <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />
+      <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23M12 19v4M8 23h8" />
+    </svg>
+  );
+}
