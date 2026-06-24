2dbfe339314241af0cb008676ef231eabc20d53c
riteshrana12-dev <ranaritesh677@gmail.com>
2026-06-24 17:12:38 +0530

    ui(icon):phone off icon

---
+++ client/src/components/video/icon/PhoneOffIcon.tsx
@@ -0,0 +1,14 @@
+export default function PhoneOffIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path d="M10.68 13.31a16 16 0 0 0 3.41 2.6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7 2 2 0 0 1 1.72 2v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A2 2 0 0 1 10.68 13.31zM1 1l22 22" />
+    </svg>
+  );
+}
